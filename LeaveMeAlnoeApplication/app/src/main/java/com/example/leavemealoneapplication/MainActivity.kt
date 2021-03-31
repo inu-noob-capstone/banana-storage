00f703410cbee1f2f50af087b2f3bf0b4e1cd40a
rkrnru <rkrnru@naver.com>
2021-03-31 17:12:13 +0900

    app update

--- LeaveMeAlnoeApplication/app/src/main/java/com/example/leavemealoneapplication/MainActivity.kt
+++ LeaveMeAlnoeApplication/app/src/main/java/com/example/leavemealoneapplication/MainActivity.kt
@@ -3,70 +3,38 @@ package com.example.leavemealoneapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.example.leavemealoneapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.editText.addTextChangedListener {
+            if(it.toString().length >= 8){
+                Log.d("EditText", "8자 이상일 때만 출력=${it.toString()}")
+            }
+        }
 
-        // 접근 제한자 테스트
-        var child = Child()
-        child.callVariables()
+        var list = mutableListOf("Scope", "Function")
 
-        // 부모 클래스 직접 호출해보기
-        var parent = Parent()
-        Log.d("Visibility", "Parent: 기본 제한자 defaultVal의 값은 ${parent.defaultVal}")
-        Log.d("Visibility", "Parent: internalVal의 값은 ${parent.internalVal}")
-    }
-}
+        val afterApply = list.let{
+            it.add("Apply")
+            it.count()
+        }
+        println("반환값 apply = $afterApply")
 
-// 추상 클래스 설계
-abstract class Animal{
-    fun walk(){
-        Log.d("abstract", "걷습니다.")
-    }
-    abstract fun move()
-}
+        val afterAlso = list.run {
+            add("Also")
+            count()
+        }
+        println("반환값 also = $afterAlso")
 
-// 구현
-class Bird:Animal(){
-    override fun move(){
-        Log.d("abstract","날아서 이동합니다.")
-    }
-}
-
-// interface 설계
-interface InterfaceKotlin {
-    var variable: String
-    fun get()
-    fun set()
-}
-
-//구현
-class KotlinImpl: InterfaceKotlin{
-    override var variable: String = "init value"
-    override fun get(){
-        // 코드 구현
-    }
-    override fun set(){
-        // 코드 구현
-    }
-}
-
-// 접근 제한자 테스트를 위한 부모 클래스
-open class Parent{
-    private val privateVal = 1
-    protected open val protectedVal = 2
-    internal val internalVal = 3
-    val defaultVal = 4
-}
-
-// 자식 클래스
-class Child: Parent(){
-    fun callVariables(){
-        // privateVal은 호출이 안 됩니다.
-        Log.d("Visibility","Child: protected의 값은 ${protectedVal}")
-        Log.d("Visibility", "Child: internalVal의 값은 ${internalVal}")
-        Log.d("Visibility", "Child: 기본 제한자 defaultVal의 값은 ${defaultVal}")
+        val lastItemWith = with(list){
+            add("With")
+            get(size-1)
+        }
+        println("반환값 with = $lastItemWith")
     }
 }
