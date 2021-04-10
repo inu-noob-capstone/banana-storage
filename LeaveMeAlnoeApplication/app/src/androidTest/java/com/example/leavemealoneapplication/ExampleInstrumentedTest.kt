470a347a42e8f6581e4d12329b0ae2ecfb007f51
unknown <rkrnru@naver.com>
2021-04-10 17:08:44 +0900

    app update...

--- LeaveMeAlnoeApplication/app/src/androidTest/java/com/example/leavemealoneapplication/ExampleInstrumentedTest.kt
+++ LeaveMeAlnoeApplication/app/src/androidTest/java/com/example/leavemealoneapplication/ExampleInstrumentedTest.kt
@@ -3,10 +3,10 @@ package com.example.leavemealoneapplication
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Test as Test1
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,7 +15,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
+    @Test1
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
