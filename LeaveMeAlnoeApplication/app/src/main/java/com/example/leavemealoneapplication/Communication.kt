68a76db855cf82e11b8f3bb66fbcbaf5ced06dee
rkrnru <rkrnru@naver.com>
2021-04-13 18:02:11 +0900

    App update

--- LeaveMeAlnoeApplication/app/src/main/java/com/example/leavemealoneapplication/Communication.kt
+++ LeaveMeAlnoeApplication/app/src/main/java/com/example/leavemealoneapplication/Communication.kt
@@ -1,11 +1,36 @@
 package com.example.leavemealoneapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.leavemealoneapplication.databinding.ActivityMainBinding
 
 class Communication : AppCompatActivity() {
+
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_communication)
+        button = findViewById(R.id.communcationBtn)
+        button.setOnClickListener(listener)
+
+        val imageView = ImageView(this)
+        imageView.setImageResource(R.drawable.unconnected)
+    }
+
+    val listener = View.OnClickListener { view ->
+        when(view.getId()){
+            R.id.communcationBtn ->{
+                startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+
+            }
+        }
     }
 }
--- LeaveMeAlnoeApplication/app/src/main/java/com/example/leavemealoneapplication/LightManagement.kt
+++ LeaveMeAlnoeApplication/app/src/main/java/com/example/leavemealoneapplication/LightManagement.kt
@@ -2,10 +2,12 @@ package com.example.leavemealoneapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.leavemealoneapplication.databinding.ActivityMainBinding
 
 class LightManagement : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_light_management)
+        setContentView(binding.root)
     }
 }
