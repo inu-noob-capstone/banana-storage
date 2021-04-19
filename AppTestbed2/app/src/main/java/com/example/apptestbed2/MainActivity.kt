e8d2549d4338cc91531134cbe399147a89c2b358
rkrnru <rkrnru@naver.com>
2021-04-20 04:50:55 +0900

    Testbed update

--- AppTestbed2/app/src/main/java/com/example/apptestbed2/MainActivity.kt
+++ AppTestbed2/app/src/main/java/com/example/apptestbed2/MainActivity.kt
@@ -6,6 +6,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.example.apptestbed2.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +24,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.buttonRequest.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    var urlText = binding.editUrl.text.toString()
+                    if (!urlText.startsWith("https")) {
+                        urlText = "https://${urlText}"
+                    }
+                    val url = URL(urlText)
+                    val urlConnection = url.openConnection() as HttpURLConnection
+                    urlConnection.requestMethod = "GET"
+
+                    if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+                        val streamReader = InputStreamReader(urlConnection.inputStream)
+                        val buffered = BufferedReader(streamReader)
+                        val content = StringBuilder()
+
+                        while (true) {
+                            val line = buffered.readLine() ?: break
+                            content.append(line)
+                        }
+
+                        buffered.close()
+                        urlConnection.disconnect()
+
+                        launch(Dispatchers.Main) {
+                            binding.textContent.text = content.toString()
+                        }
+                    }
+                } catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 
     fun serviceStart(view: View){
